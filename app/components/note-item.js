1571a35b4829184f99a9d037304bef394cec6c77
Robdel12 <robertdeluca19@gmail.com>
2015-05-22 18:07:36 -0500

    WIP: highlighting the current talked about item

--- app/components/note-item.js
+++ app/components/note-item.js
@@ -3,10 +3,26 @@ import layout from '../templates/components/note-item';
 
 export default Ember.Component.extend({
   layout: layout,
+  player: Ember.inject.service('player'),
   tagName: 'li',
+  classNameBindings: ['active'],
+  active: false,
   click(e){
     if(this.$()[0] === e.target) {
       this.$('.timestamp').trigger('click');
     }
-  }
+  },
+
+  highlightItem: Ember.observer('player.currentTime', function() {
+    var currentTime = this.get('player').get('currentTime');
+    var startTime = this.get('startTime');
+    var endTime = this.get('endTime');
+
+
+    if(startTime <= currentTime && currentTime <= endTime) {
+      this.set('active', true);
+    } else {
+      this.set('active', false);
+    }
+  })
 });
